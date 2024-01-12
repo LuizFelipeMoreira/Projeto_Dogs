0791358ed5d868d26e058e5cfa6c0551c2caa9c9
luiz-felipe-moreira <luizfelipemlds65@gmail.com>
2024-01-11 23:54:16 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
