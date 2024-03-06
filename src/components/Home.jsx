83b0c4295736090e682016192307b8cf47da5fb3
luiz-felipe-moreira <luizfelipemlds65@gmail.com>
2024-03-05 23:23:40 -0300

    atualiza tittle da pagina

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Feed from './Feed/Feed.jsx';
+import Head from './Helper/Head.jsx';
 
 const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head
+        title="Fotos"
+        description="Home do site Dogs, com o feed de fotos"
+      />
       <Feed />
     </section>
   );
