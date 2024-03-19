a5041add66944c1305702e1fa1b553e21bccd307
luiz-felipe-moreira <luizfelipemlds65@gmail.com>
2024-03-19 07:39:31 -0300

    add lazy e suspense em estatisticas

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -4,7 +4,7 @@ import useFetch from '../../Hooks/useFetch';
 import { STATS_GET } from '../../api';
 import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
-import UserStatsGraphics from './UserStatsGraphics';
+const UserStatsGraphics = React.lazy(() => import('./UserStatsGraphics'));
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +21,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="estatisticas" />
         <UserStatsGraphics data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
